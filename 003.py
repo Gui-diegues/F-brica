b91a8be59cfd87cb1841cbe9fe3b9849009e5ccf
Gui-diegues <guilhermediegues2006@gmail.com>
2025-02-21 15:30:02 -0300

    Create 003.py

---
+++ 003.py
@@ -0,0 +1,26 @@
+ #for i in range(5):
+# print('repetição número',i)
+
+#nome = ('Pedro', 'João', 'Lucas')
+#for nome in nome
+#Print (nome)
+
+#ontador = 0 
+#while contador <5:
+ #       print("contador", contador)
+  #      contador += 1 e contador
+
+
+
+
+#pessoa = {
+ #   "nome"; "carlos", 
+#    "idade"; "25",
+ #   "cidade"; "São Paulo"
+#}
+#print(pessoa)
+
+
+
+
+
