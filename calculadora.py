d15245182c331f2d0f8ef3aba0e36bc4ac9f43c6
Gui-diegues <guilhermediegues2006@gmail.com>
2025-02-21 15:39:55 -0300

    Create calculadora.py

---
+++ calculadora.py
@@ -0,0 +1,14 @@
+import math
+n1 = float(input('Digite um número:'))
+n2 = input('Digite a operação:')
+operacao = float(input('Digite outro número'))
+
+if operacao == 'somar':
+    resultado = n1 + n2
+else operacao == 'subitrair':
+    resultado = n1 - n2
+else operacao == 'dividir'
+    resultado = n1 / n2
+else operacao == 'multiplicar'
+    resultado == n1 * n2
+print({} {} {} é igual igual a: {}.format n1, n2, operacao, resultado)
